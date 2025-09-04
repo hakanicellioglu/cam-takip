56b47a7166e6afdfd618048278448c7e838dfbbc
Hakan Berke İçellioğlu <hakanicellioglu>
2025-09-04 11:31:42 +0300

    feat: create users table

---
+++ users.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `users` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(100) NOT NULL,
+  `lastname`  VARCHAR(100) NOT NULL,
+  `username`  VARCHAR(100) NOT NULL,
+  `email`     VARCHAR(255) NOT NULL,
+  `password_hash` VARCHAR(255) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uniq_username` (`username`),
+  UNIQUE KEY `uniq_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
